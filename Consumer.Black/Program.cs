e7182826304474a8b3b142914bc9432412f2bcd0
mardulius <kotel77@inbox.ru>
2022-07-24 15:19:20 +0300

    Add project files.

---
+++ Consumer.Black/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queue: queueName,
+                                     exchange: "topic_logs",
+                                     routingKey: "*.black.#");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body;
+                var message = Encoding.UTF8.GetString(body.ToArray());
+                Console.WriteLine($" Получено сообщение: {message}");
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine($"подписались на очередь {queueName}");
+            Console.WriteLine("ждем сообещние типа[*.black.#]");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+class Program
+{
+    private static readonly List<string> cars = new List<string> { "BMW", "Ford", "Volvo", "Lada" };
+    private static readonly List<string> colors = new List<string> { "red", "black", "blue", "green" };
+    private static readonly Random random = new Random();
+
+    static void Main(string[] args)
+    {
+        var counter = 0;
+
+        do
+        {
+            int timeToSleep = random.Next(1000, 2000);
+            Thread.Sleep(timeToSleep);
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            var connection = factory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("topic_logs", ExchangeType.Topic);
+
+                string routingKey = counter % 4 == 0 
+                    ?"Lada.black.fast.sport" : counter % 5 == 0
+                    ? "Volvo.exclusive.expasive.sport"
+                    :GenerateRoutingKey();
+
+                string message = $"здороува мужики, сообщение[{counter}] типа:[{routingKey}]";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: "topic_logs",
+                    routingKey: routingKey,
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine($"Сообщение [{counter++}] типа:{routingKey} - отправлено в Topic Exchange");
+            }
+        }
+        while (true);
+    }
+
+    private static string GenerateRoutingKey()
+    {
+        return $"{cars[random.Next(0, 3)]}.{colors[random.Next(0, 3)]}";
+    }
+}
